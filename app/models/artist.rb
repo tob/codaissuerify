4ddefccf361e8ee03e8fac120875c219f91bc542
tob <tobiadonati@gmail.com>
2017-07-09 02:04:24 +0200

    added tests for artist validation

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,7 @@
 class Artist < ApplicationRecord
   has_many :songs, dependent: :destroy
+
+  validates :name,    presence: true, length: { maximum: 30 }
+  validates :image_url,  presence: true
+
 end
--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,32 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Artist, type: :model do
-  describe Artist do
-    describe "#name and photo" do
-      it "is composed of name and picture" do
-        artist = build(:artist, name: "John Lee Hooker", image_url: "//some_image.jpg")
-        expect(artist.name).to eq "John Lee Hooker"
-      end
-    end
-  end
-
   describe "validations" do
-    it "is invalid without a name" do
-      artist = Artist.build(name: "")
-      artist.valid?
-      expect(artist.errors).to have_key(:name)
-    end
-
-    it "is invalid without an image_url" do
-      artist = artist.build(image_url: nil)
-      artist.valid?
-      expect(room.errors).to have_key(:image_url)
-    end
-
-    it "is invalid with a name longer than 15 characters" do
-      artist = Artist.new(listing_name: "Supercalifragilistichespiralidoso")
-      artist.valid?
-      expect(artist.errors).to have_key(:name)
-    end
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:image_url) }
+    it { is_expected.to validate_length_of(:name).is_at_most(30) }
   end
 end
