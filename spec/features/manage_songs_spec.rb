36c9c1bb8c993e7eff8b01931338079d2ab0736a
tob <tobiadonati@gmail.com>
2017-07-15 21:26:50 +0200

    added single song test - still looking for syntax

--- spec/features/manage_songs_spec.rb
+++ spec/features/manage_songs_spec.rb
@@ -1,36 +1,27 @@
 require 'rails_helper'
 
-feature 'Manage songs', js: true do
+feature 'CRUD songs', js: true do
   let(:artist) { create(:artist)}
+  let(:artist) { create(:song)}
 
-scenarios = ["add a new song", "delete a song", "delete all songs"]
+# scenarios = ["add a new song", "delete a song", "delete all songs"]
 
   scenario 'add a new song' do
     visit artist_path(artist.id)
     fill_in 'new-song', with: 'Rock around the clock via testing'
 
-    sleep(1)
+    # sleep(1)
     page.execute_script("$('form').submit()")
 
-    sleep(1)
+    # sleep(1)
     expect(page).to have_content('Rock around the clock via testing')
 
-    sleep(3)
-
-
+    # sleep(3)
   end
+  scenario 'delete single song' do
+    artist.song.last.destroy
+    expect { song.reload }.to raise_error ActiveRecord::RecordNotFound
 
-  scenario 'delete all songs' do
-    visit artist_path(artist.id)
-    #action goes here
-
-    sleep(1)
-    page.execute_script("$('form').submit()")
-
-    sleep(1)
-    expect(artist.songs.size).to eq(0)
-
-    sleep(3)
   end
 
 
