94cfd0b71ac80cf46335ccca970834f82c3a8090
tob <tobiadonati@gmail.com>
2017-07-16 18:36:00 +0200

    improved tests and fixed bugs

--- spec/features/manage_songs_spec.rb
+++ spec/features/manage_songs_spec.rb
@@ -4,8 +4,6 @@ feature 'CRUD songs', js: true do
   let(:artist) { create(:artist)}
   let(:artist) { create(:song)}
 
-# scenarios = ["add a new song", "delete a song", "delete all songs"]
-
   scenario 'add a new song' do
     visit artist_path(artist.id)
     fill_in 'new-song', with: 'Rock around the clock via testing'
@@ -15,29 +13,32 @@ feature 'CRUD songs', js: true do
 
     # sleep(1)
     expect(page).to have_content('Rock around the clock via testing')
-
-    # sleep(3)
   end
-
 
   scenario 'delete single song' do
+    visit artist_path(artist.id)
+    fill_in 'new-song', with: 'delete_this_song'
+    page.execute_script("$('form').submit()")
 
-    sngbfr_del = @artist.songs.length
+    # allows page to reload
+    sleep 1
 
-    find(".clean-this", match: :first).click
-    sngaftr_del = artist.songs.length
+    find('li', text: 'delete_this_song').click_link('Delete')
 
-    expect()
-
-
+    expect(page).to have_no_content('delete this song')
   end
+
 
   scenario 'delete all songs' do
     visit artist_path(artist.id)
+    for  song in 0..3
+      fill_in 'new-song', with: 'Same old song'
+      page.execute_script("$('form').submit()")
+      sleep 1
+    end
 
-    click_link('clean-up')
+    click_button('clean-up')
     expect(page).to have_no_css('.song')
-
   end
 
 end
